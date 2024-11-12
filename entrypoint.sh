cca63db2e9c7df60b90a9b6648bcaacc8600e1ea
nloft <147839433+nloft@users.noreply.github.com>
2024-11-11 22:11:54 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -e
 
-ollama pull llama3.2:3b-instruct-fp16
-ollama pull llama3.1:8b-instruct-q6_K
-ollama serve & sleep 3
+# Adapt to GPU presence.
+if command -v nvidia-smi &> /dev/null && nvidia-smi -L | grep -q "GPU"; then
+    echo "GPU detected. Selecting larger model."
+    SELECTED_MODEL="llama3.1:8b-instruct-q6_K"
+else
+    echo "No GPU detected. Selecting smaller model."
+    SELECTED_MODEL="llama3.2:3b-instruct-fp16"
+fi
 
+# Update environment model.
+echo "SELECTED_MODEL=$SELECTED_MODEL" > ../.env
+
+# Pull the selected model
+ollama pull "$SELECTED_MODEL"
+
+# Start and keep alive Ollama.
+ollama serve & sleep 5
 wait
